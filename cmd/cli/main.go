90890eb0a9992e928a39b1c2b993b9e292567b7b
Raúl Barroso <ra.barroso@gmail.com>
2024-06-04 18:04:32 +0200

    add version

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,12 +1,16 @@
 package cli
 
 import (
+	"context"
+	"fmt"
+	"os"
+
 	"github.com/conduitio/conduit/pkg/conduit"
 	"github.com/spf13/cobra"
 )
 
-func Run() {
-	var cmdServe = &cobra.Command{
+func cmdServe() *cobra.Command {
+	return &cobra.Command{
 		Use:   "start",
 		Short: "Start conduit service",
 		Long:  `Starts the conduit service. This command will start the conduit service`,
@@ -15,8 +19,33 @@ func Run() {
 			conduit.Serve(conduit.DefaultConfig())
 		},
 	}
+}
 
+func cmdVersion() *cobra.Command {
+	return &cobra.Command{
+		Use:   "version",
+		Short: "Conduit version",
+		Run: func(cmd *cobra.Command, args []string) {
+			// TODO: Show when was last updated
+			_, _ = fmt.Fprintf(os.Stdout, "%s\n", conduit.Version(true))
+		},
+	}
+}
+
+func Cmd() *cobra.Command {
 	var rootCmd = &cobra.Command{Use: "conduit"}
-	rootCmd.AddCommand(cmdServe)
-	rootCmd.Execute()
+
+	// Add commands
+	rootCmd.AddCommand(cmdServe(), cmdVersion())
+
+	return rootCmd
+}
+
+func Run() {
+	ctx := context.Background()
+
+	rootCmd := Cmd()
+	if err := rootCmd.ExecuteContext(ctx); err != nil {
+		os.Exit(1)
+	}
 }
